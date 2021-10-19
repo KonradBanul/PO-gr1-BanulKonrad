75056ce821ae2410b03b0c5a19cba3259f0147d1
KonradBanul <73536807+KonradBanul@users.noreply.github.com>
2021-10-19 16:17:17 +0200

    Initial

--- lab01/Zadanie21h.java
+++ lab01/Zadanie21h.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
-public class Zadanie2 {
+public class Zadanie21h {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int i = 0;
-        int m = in.nextInt();
-        while(i != n - 1) {
+        int ile = 0;
+        while(i != n) {
             int k = in.nextInt();
-            System.out.println(k);
+            if(k < 0)
+                k *= -1;
+            if(k < Math.pow((i+1), 2))
+                ile++;
             i++;
         }
-        System.out.println(m);
+        System.out.println(ile);
     }
 }
