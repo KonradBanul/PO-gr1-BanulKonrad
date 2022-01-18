df9e8ac25f82adb1d22006267ad8752c0e062887
KonradBanul <73536807+KonradBanul@users.noreply.github.com>
2022-01-18 15:42:01 +0100

    Delete Zad10.java

--- Zad10.java
+++
@@ -1,8 +0,0 @@
-public class Zad10 {
-    public static void main(String[] args) {
-        System.out.println("Forrest Gump");
-        System.out.println("Zielona Mila");
-        System.out.println("Ojciec Chrzestny");
-    }
-
-}
