3d6b15b464f2b0d1b50394e247a0f90ff4ff3440
KonradBanul <73536807+KonradBanul@users.noreply.github.com>
2022-01-18 15:41:44 +0100

    Delete Zad07.java

--- Zad07.java
+++
@@ -1,9 +0,0 @@
-public class Zad07 {
-    public static void main(String[] args) {
-        System.out.println("*     *    ***     *     *  * * *       *      * *        * * *       *      *     *  *     *  *    ");
-        System.out.println("*   *    *     *   * *   *  *    *     * *     *   *      *    *     * *     * *   *  *     *  *    ");
-        System.out.println("* *     *       *  *  *  *  * * *     *   *    *    *     * * *     *   *    *  *  *  *     *  *    ");
-        System.out.println("*  *     *     *   *   * *  *   *    * * * *   *   *      *    *   * * * *   *   * *   *   *   *    ");
-        System.out.println("*    *     ***     *     *  *    *  *       *  *  *       * * *   *       *  *     *    * *    * * *");
-    }
-}
